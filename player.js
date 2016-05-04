4c9246741a84aebacc13cd559806fb23c504b351
irontongue <irontongue101@hotmail.com>
2016-05-04 18:16:02 +0930

    Sprite.js error

--- player.js
+++ player.js
@@ -1,27 +1,56 @@
-var Player = function () {
-    this.image = document.createElement("img");
+var Player = function () 
+{
+    this.sprite = new Sprite("ChuckNorris.png");
+    this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+    [0, 1, 2, 3, 4, 5, 6, 7]);
+    this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+    [8, 9, 10, 11, 12]);
+    this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+    [13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]);
+    this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+    [52, 53, 54, 55, 56, 57, 58, 59]);
+    this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+    [60, 61, 62, 63, 64]);
+    this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+    [65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78]);
+    
+    for(var i=0; i<ANIM_MAX; i++)
+    {
+        this.sprite.setAnimationOffset(i, -55, -87);
+        
+    }
+    
     this.position = new Vector2();
     this.position.Set (9 * TILE, 0 * TILE);
 
     this.width = 159;
     this.height = 163;
 
-    this.offset = new Vector2();
-    this.offset.Set(-55, -87);
-
     this.velocity = new Vector2();
     this.velocity.Set(0,0);
     this.falling = true;
     this.jumping = false;
 
-    this.image.src = "hero.png";
+    
 };
 
 
+var LEFT = 0;
+var RIGHT = 1;
+
+var ANIM_IDLE_LEFT = 0;
+var ANIM_JUMP_LEFT = 0;
+var ANIM_WALK_LEFT = 0;
+var ANIM_IDLE_RIGHT = 3;
+var ANIM_JUMP_RIGHT = 4;
+var ANIM_WALK_RIGHT = 5;
+var ANIM_MAX = 6;
 
 
     Player.prototype.update = function (deltaTime)
  {
+     this.sprite.update(deltaTime);
+     
      var METER = TILE;
      var GRAVITY = METER *9.8 * 6;
      var MAXDX = METER * 10;
@@ -132,8 +161,9 @@ Player.prototype.draw = function()
 {
 	    context.save();			
 		context.translate(this.position.x, this.position.y);
-		context.drawImage(this.image, -55,-90);	
+		context.drawImage(this.image, -50,-90);	
 	    context.restore();
-        context.fillStyle = '#0000ff'	
+        this.sprite.draw(context, this.position.x, this.position.y);
+       // context.fillStyle = '#0000ff'	
        // context.fillRect(player.position.x, player.position.y, 35, 35)
 }
